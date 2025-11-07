73eab1ddddec147ab32017937c4d3d2d2046b4ed
rohitwaghmare7470 <rohitwaghmare7470@gmail.com>
2025-11-08 04:28:30 +0530

    this is the first version of test file

--- tests/CrewBloomYouTube.spec.js
+++ tests/CrewBloomYouTube.spec.js
@@ -15,5 +15,20 @@ test('@YouTube end-to-end video flow', async ({ page }) => {
   await crewbloomyoutube.navigate();
   await expect(page).toHaveURL(/youtube\.com/);
 
+  //test.soft.expect(await page.title()).toContain('YouTube');
+
+
+
+// Step 2: Searching for a keyword
+  await crewbloomyoutube.navigate();
+  await crewbloomyoutube.search('QA automation');
+  await crewbloomyoutube.searchButton.click();
+await page.waitForTimeout(3000);
+
+  // Step 3: Click on the first video link
+  await crewbloomyoutube.clickFirstVideo();
+ await test.soft.expect(count, 'Expected at least one video result').toBeGreaterThan(0);
+
+
  
 });
