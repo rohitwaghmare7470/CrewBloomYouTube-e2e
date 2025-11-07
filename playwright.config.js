198296135cbe8f0c5827fa4a4d882ce69dd958f8
rohitwaghmare7470 <rohitwaghmare7470@gmail.com>
2025-11-08 02:13:39 +0530

    this is the initial commit for playwright config and package steup

---
+++ playwright.config.js
@@ -0,0 +1,21 @@
+// @ts-check
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000, // 30 seconds per test
+  expect: {
+    timeout: 5000 // timeout for expect conditions
+  },
+  fullyParallel: true,
+  retries: 0,
+  reporter: [['list'], ['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 10000,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'on-first-retry'
+  },
+});
