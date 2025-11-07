1e247a46e33b445a1412dc1937fd20fe5f700fc2
rohitwaghmare7470 <rohitwaghmare7470@gmail.com>
2025-11-08 04:27:45 +0530

    this is the code which needs to be improved

---
+++ pageobjects/CrewBloomYouTube.js
@@ -0,0 +1,41 @@
+class CrewBloomYouTube {
+  constructor(page) {
+    this.page = page;
+    this.searchBox = page.locator("input[placeholder*='Search']");
+    this.searchButton = page.locator("button[title='Search'] div");
+    this.videoList = page.locator("a[class*='yt-core-attributed-string__link yt-core-attributed-string__link--call-to-action-color yt-core-attributed-string--link-inherit-color']");
+    
+    this.videoPlayer = page.locator("ytd-search-pyv-renderer[class='style-scope ytd-item-section-renderer']");
+    this.videoTitle = page.locator("h1.title");
+  }
+
+
+  async navigate() {
+    await this.page.goto('https://www.youtube.com/');
+    await this.page.waitForLoadState('domcontentloaded');
+  }
+
+async search(keyword) {
+  
+  await this.searchBox.fill(keyword, { timeout: 10000 });
+  await this.searchButton.hover({timeout: 1000 });
+  
+  await this.searchButton.waitFor({ state :'visible',timeout: 10000 });
+;
+
+  await this.page.waitForLoadState('domcontentloaded');
+}
+
+async clickFirstVideo() {
+  // Hover over the first video result
+  await this.videoList.first().hover({ timeout: 2000 });
+  // Click only the first video result
+  await this.videoList.first().click({ timeout: 5000 });
+  await this.page.waitForLoadState('domcontentloaded');
+}
+
+
+
+}
+
+module.exports = CrewBloomYouTube;
