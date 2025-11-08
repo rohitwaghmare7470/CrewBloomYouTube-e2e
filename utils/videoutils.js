14bfb044ab4fbbeb58fd90bccf0ade06d4760081
rohitwaghmare7470 <rohitwaghmare7470@gmail.com>
2025-11-09 00:36:56 +0530

    This is the final code

--- utils/videoutils.js
+++ utils/videoutils.js
@@ -7,4 +7,26 @@ module.exports = {
   },
 
 
+    async waitForVideoToStart(page, timeout = 10000) {
+    await page.waitForFunction(() => {
+      const video = document.querySelector('video');
+      return video && video.currentTime > 0;
+    }, { timeout });
+  },
+
+async togglePlayPause(page) {
+    await page.waitForTimeout(4000);
+    await page.keyboard.press('k');
+    await page.waitForTimeout(4000);
+  },
+
+  async skipForward(page, seconds = 10) {
+    for (let i = 0; i < seconds / 5; i++) {
+      await page.keyboard.press('ArrowRight');
+      await page.waitForTimeout(500);
+    }
+  }
+
+
+
 };
