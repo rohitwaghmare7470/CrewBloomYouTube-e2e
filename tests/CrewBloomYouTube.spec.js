8cc08abd067833862728c2b5f66bc2b3be107a41
rohitwaghmare7470 <rohitwaghmare7470@gmail.com>
2025-11-09 00:05:32 +0530

    nearly done with code

--- tests/CrewBloomYouTube.spec.js
+++ tests/CrewBloomYouTube.spec.js
@@ -16,16 +16,12 @@ test('@YouTube end-to-end video flow', async ({ page }) => {
   await expect(page).toHaveURL(/youtube\.com/);
 
   //test.soft.expect(await page.title()).toContain('YouTube');
-
-
-
+  expect.soft(await page.title(), 'Page title should contain YouTube').toContain('YouTube');
 // Step 2: Searching for a keyword
   await crewbloomyoutube.navigate();
   await crewbloomyoutube.search('QA automation');
   await crewbloomyoutube.searchButton.click();
 await page.waitForTimeout(3000);
-
- 
 
 
   // Step 3: Click on the first video link
@@ -33,9 +29,33 @@ await page.waitForTimeout(3000);
   const videoCount = await crewbloomyoutube.videoList.count();
   expect.soft(videoCount, 'Expected at least one video result').toBeGreaterThan(0);
   //video count validation failed
-  
-  
  //await test.soft.expect(count, 'Expected at least one video result').toBeGreaterThan(0);
+
+ const timeBefore = await crewbloomyoutube.getVideoTime();
+  await crewbloomyoutube.page.waitForTimeout(2000);
+  await crewbloomyoutube.page.keyboard.press('ArrowRight');
+  await crewbloomyoutube.page.waitForTimeout(2000);
+  const timeAfter = await crewbloomyoutube.getVideoTime();
+  expect.soft(timeAfter).toBeGreaterThan(timeBefore);
+
+ await crewbloomyoutube.playPauseAndSmartSkip();
+
+ 
+
+  const screenshotTaken = await crewbloomyoutube.takeScreenshot();
+  expect.soft(screenshotTaken).toBe(true);
+
+  const title = await crewbloomyoutube.getVideoTitle();
+  expect.soft(title.trim().length).toBeGreaterThan(0);
+
+
+  crewbloomyoutube.handleAdIfPresent();
+  crewbloomyoutube.getVideoTime();
+  crewbloomyoutube.playPauseAndSmartSkip();
+  
+
+  //await browser.close();
+
 
 
  
